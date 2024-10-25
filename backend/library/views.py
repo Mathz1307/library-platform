ddd2579a415baee052b5f66f19da73ace243a07d
Mateus Flamínio <mflaminio482@gmail.com>
2024-10-25 16:06:35 +0100

    forgot to uncomment get_books lines

--- backend/library/views.py
+++ backend/library/views.py
@@ -11,20 +11,20 @@ def get_books(request):
     books = Book.objects.all()
     books_data = BookSerializer(books, many=True).data
 
-    #authors_id = [tuple(books_data[i]["authors"]) for i in range(len(books_data))]
-    #genres_id = [tuple(books_data[i]["authors"]) for i in range(len(books_data))]
+    authors_id = [tuple(books_data[i]["authors"]) for i in range(len(books_data))]
+    genres_id = [tuple(books_data[i]["authors"]) for i in range(len(books_data))]
 
-    #for i in range(len(authors_id)):
-    #    authors = Author.objects.filter(id__in=authors_id[i])
-    #    authors_data = AuthorSerializer(authors, many=True).data
+    for i in range(len(authors_id)):
+        authors = Author.objects.filter(id__in=authors_id[i])
+        authors_data = AuthorSerializer(authors, many=True).data
 
-    #    books_data[i]["authors"] = authors_data
+        books_data[i]["authors"] = authors_data
 
-    #for i in range(len(genres_id)):
-    #    genres = Genre.objects.filter(id__in=genres_id[i])
-    #    genres_data = GenreSerializer(genres, many=True).data
+    for i in range(len(genres_id)):
+        genres = Genre.objects.filter(id__in=genres_id[i])
+        genres_data = GenreSerializer(genres, many=True).data
 
-    #    books_data[i]["genres"] = genres_data
+        books_data[i]["genres"] = genres_data
     
 
     return Response(books_data)
